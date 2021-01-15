78ccb18f1563d2b04236271793202cb281c5cf61
coo11 <22176261+coo11@users.noreply.github.com>
2021-01-16 01:39:47 +0800

    Update

--- QuantumultX/RewriteSinaShortUrl.js
+++ QuantumultX/RewriteSinaShortUrl.js
@@ -1,10 +1,10 @@
 (() => {
   let headers = $response.headers;
-  headers = JSON.stringify(headers);
+  headers = JSON.parse(headers);
   if ("Location" in headers) {
     headers["Http-Status-Code"] = 302;
   }
   console.log("新浪短链接重定向", headers)
-  headers = JSON.parse(headers);
+  headers = JSON.stringify(headers);
   $done({ headers });
 })();
