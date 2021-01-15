df21db7c847db187d6071b492cd18891715b42ac
coo11 <22176261+coo11@users.noreply.github.com>
2021-01-16 01:51:52 +0800

    Fix

--- QuantumultX/RewriteSinaShortUrl.js
+++ QuantumultX/RewriteSinaShortUrl.js
@@ -1,10 +1,9 @@
 (() => {
   let headers = $response.headers;
-  headers = JSON.parse(headers);
   if ("Location" in headers) {
     headers["Http-Status-Code"] = 302;
-  }
-  console.log("新浪短链接重定向", headers)
-  headers = JSON.stringify(headers);
-  $done({ headers });
+    console.log("新浪短链接重定向", JSON.stringify(headers));
+    const status = "HTTP/1.1 302 Found";
+    $done({ status, headers });
+  } else $done({});
 })();
