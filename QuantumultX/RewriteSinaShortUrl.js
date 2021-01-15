0657305757c7d0115aca89115e3c4e9975b07ac9
coo11 <22176261+coo11@users.noreply.github.com>
2021-01-16 02:19:01 +0800

    Fix t.cn|sinaurl.cn redirect

--- QuantumultX/RewriteSinaShortUrl.js
+++ QuantumultX/RewriteSinaShortUrl.js
@@ -1,9 +1,6 @@
 (() => {
   let headers = $response.headers;
   if ("Location" in headers) {
-    headers["Http-Status-Code"] = 302;
-    console.log("新浪短链接重定向", JSON.stringify(headers));
-    const status = "HTTP/1.1 302 Found";
-    $done({ status, headers });
+    $done({ status: "HTTP/1.1 302 Found" });
   } else $done({});
 })();
