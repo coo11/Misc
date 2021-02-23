fe9682f841cff41e6dafa33f5325d7e107217fb4
coo11 <22176261+coo11@users.noreply.github.com>
2021-02-23 20:44:52 +0800

    Bug fix

--- QuantumultX/BingImageReverse.js
+++ QuantumultX/BingImageReverse.js
@@ -1,7 +1,8 @@
 (() => {
-  let headers = $request.headers;
+  let { headers, path } = $request;
   headers["User-Agent"] =
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15";
-  // if (path.startsWith("/images/"))
-  $done({ headers });
+  if (/imgurl=https?%3A%2F%2F(i(?:-c?f)\.pximg\.net)%2F/.test(path))
+    $done({ path: path.replace(RegExp.$1, "i.pximg.cat"), headers });
+  else $done({});
 })();
