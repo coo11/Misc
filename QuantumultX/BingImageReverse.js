ca24ade180a4862fe8ae6ace71cf3e9ce0e479d3
coo11 <22176261+coo11@users.noreply.github.com>
2021-04-03 01:31:35 +0800

    ttkg

--- QuantumultX/BingImageReverse.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  let headers = $request.headers;
-  headers["User-Agent"] =
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15";
-  // if (path.startsWith("/images/"))
-  $done({ headers });
-})();
