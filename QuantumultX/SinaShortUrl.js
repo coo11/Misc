4db2a84c98d5d54239cbfab9047107ad0bc69e43
coo11 <22176261+coo11@users.noreply.github.com>
2021-09-27 20:22:39 +0800

    Update

--- QuantumultX/SinaShortUrl.js
+++ QuantumultX/SinaShortUrl.js
@@ -2,6 +2,5 @@
   let headers = $response.headers;
   if ("Location" in headers && $response.statusCode != 302) {
     $done({ status: "HTTP/1.1 302 Found" });
-    return;
-  } else $done({});
+  } else $done();
 })();
