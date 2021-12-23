ab9f9f8e9e710f461cad786549406d34faa63b31
coo11 <22176261+coo11@users.noreply.github.com>
2021-12-24 02:14:45 +0800

    2021 Last Update

--- QuantumultX/Referer.js
+++ QuantumultX/Referer.js
@@ -2,6 +2,7 @@
   let url = $request.url,
     headers = $request.headers;
   if (headers.Host.endsWith(".pximg.net")) {
+    headers.Host = 'i-cf.pximg.net';
     headers["Referer"] = "https://www.pixiv.net/";
     $done({ headers });
   } else $done({});
