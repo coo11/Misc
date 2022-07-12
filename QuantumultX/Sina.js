f4347ba93aaa656a262635f33ea72fe83b1b67b2
coo11 <22176261+coo11@users.noreply.github.com>
2022-07-12 10:40:03 +0800

    Update rewrite test 2

--- QuantumultX/Sina.js
+++ QuantumultX/Sina.js
@@ -7,14 +7,18 @@
     case /toasturl=([^&]+)/.test(url):
       headers["Location"] = decodeURIComponent(RegExp.$1)
       $done({ status: "HTTP/1.1 302 Found", headers });
+      break;
     case /^https?:\/\/(t|sinaurl)\.cn\//.test(url):
       if ("Location" in headers && $response.statusCode != 302) {
         $done({ status: "HTTP/1.1 302 Found" });
       } else $done();
+      break;
     case /^https?:\/\/service\.account\.weibo\.com\/reportspamobile\?.*?rid=(\d+)/.test(url):
-      body = body.replace(/c_c_a">/, $ & <a href="javascript:navigator.share({title: 'Weibo Share', url: 'https://m.weibo.cn/status/${RegExp.$1}'})" class="m-btn-orange" style="line-height: 0.5rem; border-radius: 2px; padding: 0px 8px; float: right; margin: 0.12rem 0; font-size: 0.24rem;">\u4e0d\u6295\u8bc9\uff0c\u4ec5\u5206\u4eab</a>);
+      body = body.replace(/c_c_a">/, `$&<a href="javascript:navigator.share({title: 'Weibo Share', url: 'https://m.weibo.cn/status/${RegExp.$1}'})" class="m-btn-orange" style="line-height: 0.5rem; border-radius: 2px; padding: 0px 8px; float: right; margin: 0.12rem 0; font-size: 0.24rem;">\u4e0d\u6295\u8bc9\uff0c\u4ec5\u5206\u4eab</a>`);
       $done({ body });
+      break;
     default:
       $done()
+      break;
   }
 })();
