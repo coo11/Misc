8032e8c29dfab048fab99857eb3187af8a00c10a
cooll <22176261+coo11@users.noreply.github.com>
2023-11-01 21:18:25 +0800

    Bug fix

--- QuantumultX/SauceNAO.js
+++ QuantumultX/SauceNAO.js
@@ -9,7 +9,7 @@
     .replace(
       "</head>",
       "<style>" +
-        "#headerbarright,.resultimage{float:unset;text-align:center}body{font-size:87.5%;font-family:Verdana,Helvetica,sans-serif}#headerarea,#mainarea{width:100%;min-width:unset}#headerbarright{width:100%;direction:unset}#mainarea,.resultmatchinfo,.resulttable tr,.resulttablecontent{display:flex;flex-direction:column;align-items:center}#left{width:100%;background-color:#1d1d1d}#yourimagecontainer{padding-top:0;text-align:center}#yourimage,#yourimagetext{display:inline-block;float:unset}#yourimage img,.resultimage img{max-width:unset}#yourimageretrylinks{display:flex;justify-content:center;flex-direction:row;margin:8px;gap:16px}#middle{margin:unset;width:100%}.result{margin:0 5px 5px}.resulttableimage{background-color:unset}.pixelated{width:unset;min-height:150px}.resultmatchinfo{row-gap:4px;margin:0}.resultmiscinfo{display:flex;flex-direction:row;align-items:flex-start;gap:8px}.resultcontent{width:100%}.resulttitle{margin-bottom:0}#footerarea,#headerbarleft,#headerbarmiddle,#smalllogo,#yourimageretrylinks>div,.resultcontentcolumn br:last-of-type{display:none}" +
+        '#headerbarright,.resultimage{float:unset;text-align:center}body{font-size:87.5%;font-family:Verdana,Helvetica,sans-serif}#headerarea,#mainarea{width:100%;min-width:unset}#headerbarright{width:100%;direction:unset}#mainarea,.resultmatchinfo,.resulttable tr,.resulttablecontent{display:flex;flex-direction:column;align-items:center}#left{width:100%;background-color:#1d1d1d}#yourimagecontainer{padding-top:0;text-align:center}#yourimage{display:inline-block;float:none;position:relative;min-height:150px}#yourimage::before{content:"click to edit your image";position:absolute;bottom:0;left:0;font-size:smaller;background-color:rgba(0,0,0,.5);backdrop-filter:blur(2px)}#yourimageretrylinks{display:flex;justify-content:center;flex-direction:row;margin:8px;gap:16px}#middle{margin:unset;width:100%}.result{margin:0 5px 5px}.resulttableimage{background-color:unset}.resultimage{min-width:unset}.resultimage_showmessage::after{left:0;padding:unset}.pixelated{width:unset;min-height:150px}.resultmatchinfo{row-gap:4px;margin:0}.resultmiscinfo{display:flex;flex-direction:row;align-items:flex-start;gap:8px}.resultcontent{width:100%}.resulttitle{margin-bottom:0}#footerarea,#headerbarleft,#headerbarmiddle,#smalllogo,#yourimageretrylinks>div,#yourimagetext,.resultcontentcolumn br:last-child{display:none}' +
         "</style></head>"
     );
   $done({ body });
@@ -53,15 +53,22 @@ body {
     text-align: center;
 }
 
-#yourimage,
-#yourimagetext {
+#yourimage {
     display: inline-block;
-    float: unset;
+    float: none;
+    position: relative;
+    min-height: 150px;
 }
 
-#yourimage img,
-.resultimage img {
-    max-width: unset;
+#yourimage::before {
+    content: "click to edit your image";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    font-size: smaller;
+    background-color: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(2px);
+
 }
 
 #yourimageretrylinks {
@@ -88,6 +95,12 @@ body {
 .resultimage {
     float: unset;
     text-align: center;
+    min-width: unset;
+}
+
+.resultimage_showmessage::after {
+    left: 0;
+    padding: unset;
 }
 
 .pixelated {
@@ -117,9 +130,10 @@ body {
 
 #headerbarleft,
 #headerbarmiddle,
+#yourimagetext,
 #yourimageretrylinks>div,
 #smalllogo,
-.resultcontentcolumn br:last-of-type,
+.resultcontentcolumn br:last-child,
 #footerarea {
     display: none;
 }
