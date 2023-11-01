876a1535cfa00e4ce23454f6d3e274597dc0405e
cooll <22176261+coo11@users.noreply.github.com>
2023-11-01 18:33:45 +0800

    Minor bug fix

--- QuantumultX/SauceNAO.js
+++ QuantumultX/SauceNAO.js
@@ -8,8 +8,9 @@
     )
     .replace(
       "</head>",
-      "#headerbarright,.resultimage{float:unset;text-align:center}#headerbarright,#left,#middle,.resultcontent{width:100%}body{font-size:87.5%;font-family:Verdana,Helvetica,sans-serif}#headerarea,#mainarea{width:100%;min-width:unset}#headerbarright{direction:unset}#mainarea,.resultmatchinfo,.resulttable tr,.resulttablecontent{display:flex;flex-direction:column;align-items:center}#left{background-color:#1d1d1d}#yourimagecontainer{padding-top:0;text-align:center}#yourimage,#yourimagetext{display:inline-block;float:none}#yourimage img,.resultimage img{max-width:unset}#yourimageretrylinks{display:flex;justify-content:center;flex-direction:row;margin:8px;gap:16px}#middle{margin:unset}.result{margin:0 5px 5px}.resulttableimage{background-color:unset}.resultmatchinfo{row-gap:4px;margin:0}.resultmiscinfo{display:flex;flex-direction:row;align-items:flex-start;gap:8px}.resulttitle{margin-bottom:0}#footerarea,#headerbarleft,#headerbarmiddle,#smalllogo,#yourimageretrylinks>div,.resultcontentcolumn br:last-of-type{display:none}" +
-        "</head>"
+      "<style>" +
+        "#headerbarright,.resultimage{float:unset;text-align:center}#headerbarright,#left,#middle,.resultcontent{width:100%}body{font-size:87.5%;font-family:Verdana,Helvetica,sans-serif}#headerarea,#mainarea{width:100%;min-width:unset}#headerbarright{direction:unset}#mainarea,.resultmatchinfo,.resulttable tr,.resulttablecontent{display:flex;flex-direction:column;align-items:center}#left{background-color:#1d1d1d}#yourimagecontainer{padding-top:0;text-align:center}#yourimage,#yourimagetext{display:inline-block;float:none}#yourimage img,.resultimage img{max-width:unset}#yourimageretrylinks{display:flex;justify-content:center;flex-direction:row;margin:8px;gap:16px}#middle{margin:unset}.result{margin:0 5px 5px}.resulttableimage{background-color:unset}.resultmatchinfo{row-gap:4px;margin:0}.resultmiscinfo{display:flex;flex-direction:row;align-items:flex-start;gap:8px}.resulttitle{margin-bottom:0}#footerarea,#headerbarleft,#headerbarmiddle,#smalllogo,#yourimageretrylinks>div,.resultcontentcolumn br:last-of-type{display:none}" +
+        "</style></head>"
     );
   $done({ body });
 })();
